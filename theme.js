54dfe1ec2e90c971cdc665d85cad13f8d29332dd
lusdrey523 <lusdreyteruzzi@gmail.com>
2026-05-11 21:10:55 -0400

    Delete theme.js

--- theme.js
+++
@@ -1,9 +0,0 @@
-export function loadTheme() {
-  const t = localStorage.getItem("theme") || "dark";
-  document.documentElement.setAttribute("data-theme", t);
-}
-
-export function setTheme(t) {
-  localStorage.setItem("theme", t);
-  document.documentElement.setAttribute("data-theme", t);
-}
